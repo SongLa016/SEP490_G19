07c8a6e3b83368073336fcc85783dd2a87fcb7d3
Nguyễn Sông La <songla01062003@gmail.com>
2025-10-07 19:57:47 +0700

    Create FieldRepository.cs

---
+++ Back-end/SEP490_G19/BallSport.Infrastructure/Repositories/FieldRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BallSport.Infrastructure.Data;
+using BallSport.Infrastructure.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BallSport.Infrastructure.Repositories
+{
+    // Loại sân (5,7,9,11 người)
+    public class FieldRepository
+    {
+        private readonly Sep490G19v1Context _context;
+
+        public FieldRepository(Sep490G19v1Context context)
+        {
+            _context = context;
+        }
+        public async Task<Field> AddAsync(Field field)
+        {
+            _context.Fields.Add(field);
+            await _context.SaveChangesAsync();
+            return field;
+        }
+
+        public async Task<List<Field>> GetByTypeIdAsync(int typeId)
+        {
+            return await _context.Fields
+                .Where(f => f.TypeId == typeId)
+                .ToListAsync();
+        }
+    }
+}
