b436cc05d07dcd2afae4cc945845d311334f2e25
Nguyễn Sông La <songla01062003@gmail.com>
2025-11-22 09:54:30 +0700

    sửa dùng cloundianary

--- Back-end/SEP490_G19/BallSport.Infrastructure/Models/FieldImage.cs
+++ Back-end/SEP490_G19/BallSport.Infrastructure/Models/FieldImage.cs
@@ -1,19 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BallSport.Infrastructure.Models
+namespace BallSport.Infrastructure.Models;
+
+public partial class FieldImage
 {
-    public class FieldImage
-    {
-        [Key]
-        public int ImageId { get; set; }
-        public int FieldId { get; set; }
-        public byte[] Image { get; set; } = null!;
+    public int ImageId { get; set; }
 
-        public Field Field { get; set; } = null!;
-    }
+    public int FieldId { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public virtual Field Field { get; set; } = null!;
 }
