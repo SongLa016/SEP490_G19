e799106d6d4251609b2a67798e5e0ca0dd8f897b
Ductrung2923 <ndtrung292003@gmail.com>
2025-12-04 23:32:12 +0700

    get booking month

---
+++ Back-end/SEP490_G19/BallSport.Application/DTOs/PSDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallSport.Application.DTOs
+{
+    public class PSDTO
+    {
+        public int PackageSessionId { get; set; }
+        public DateOnly SessionDate { get; set; }
+        public decimal PricePerSession { get; set; }
+        public string? SessionStatus { get; set; }
+        public int UserId { get; set; }
+        public int ScheduleId { get; set; }
+
+       
+    }
+}
